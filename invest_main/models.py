20da1625ff6731a1b7240b48de70c80e01d96dcf
captainluzik <dmitriy.luzanovsky@gmail.com>
2017-01-24 23:02:52 +0200

    Добавлен новостной модуль. Оптимизация БД под новости

--- invest_main/models.py
+++ invest_main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topics(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+
+class Topic(models.Model):
+    title = models.ForeignKey(Topics, verbose_name='Тема')
+    sub_title = models.CharField(max_length=100, verbose_name='Заголовок')
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    text = models.TextField(verbose_name='Текст статьи')
+
+
